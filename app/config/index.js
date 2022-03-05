fe22c3a79cb09a8c2d905c5299a547362e7435eb
compiler3107 <phuocthang1307@gmail.com>
2022-03-05 16:11:57 +0700

    Cai dat express chay thong bao xin chao

---
+++ app/config/index.js
@@ -0,0 +1,9 @@
+const config = {
+    app: {
+        port: process.env.port || 8005,
+    },
+    // db:{
+    //     uri: process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.r5ss6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    // }
+}
+module.exports = config;
