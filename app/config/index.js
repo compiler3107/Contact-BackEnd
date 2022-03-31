f37f5a5907ad91ad0f71905d31030f27a90a2007
compiler3107 <phuocthang1307@gmail.com>
2022-03-31 14:43:55 +0700

    Hien thi danh sach cac contact lay ve tu server

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     app: {
-        port: process.env.port || 8005,
+        port: process.env.port || 8080,
     },
     db:{
         uri: process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.r5ss6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
